950644f92b16b1f2b07a97caa09222b65d9183cc
Max Brauer <ma.brauer@live.de>
2024-08-15 21:43:29 +0200

    Update test tools

--- Test/Tools/Runner.cs
+++ Test/Tools/Runner.cs
@@ -3,177 +3,167 @@ using System.Collections.Generic;
 using System.Linq;
 using Test.Tools.User;
 using Werewolf.Theme;
-using Werewolf.Theme.User;
 using Werewolf.User;
 
-namespace Test.Tools
+namespace Test.Tools;
+
+public class Runner<TMode>
+    where TMode : GameMode
 {
-    public class Runner<T>
-        where T : GameMode
+    public TMode Mode { get; }
+
+    public GameRoom GameRoom { get; }
+
+    private readonly TestUserFactory factory;
+
+    private readonly object lockCapturedEvents = new();
+    private readonly List<GameEvent> capturedEvents = [];
+
+    private static TMode Construct(GameRoom room, UserFactory factory)
     {
-        public T Theme { get; }
+        var constructed = Activator.CreateInstance(typeof(TMode), room, factory);
+        IsInstanceOfType<TMode>(
+            constructed,
+            $"{typeof(TMode).FullName} has no constructor that accepts {typeof(GameRoom).FullName} " +
+            $"and {typeof(UserFactory)} as arguments"
+        );
+        return (TMode)constructed;
+    }
 
-        public GameRoom GameRoom { get; }
+    public Runner()
+        : this(Construct)
+    { }
 
-        private readonly TestUserFactory factory;
-
-        private readonly object lockCapturedEvents = new object();
-        private readonly List<GameEvent> capturedEvents = new List<GameEvent>();
-
-        private static T Construct(GameRoom room, UserFactory factory)
+    public Runner(Func<GameRoom, UserFactory, TMode> themeConstructor)
+    {
+        factory = new TestUserFactory();
+        GameRoom = new GameRoom(0, factory.NewUser())
         {
-            var constructed = Activator.CreateInstance(typeof(T), room, factory);
-            if (constructed is T theme)
-                return theme;
-            else throw new InvalidOperationException(
-                $"{typeof(T).FullName} has no constructor that accepts {typeof(GameRoom).FullName} " +
-                $"and {typeof(UserFactory)} as arguments"
-            );
-        }
-
-        public Runner()
-            : this(Construct)
-        { }
-
-        public Runner(Func<GameRoom, UserFactory, T> themeConstructor)
+            LeaderIsPlayer = false,
+        };
+        Mode = themeConstructor(GameRoom, factory);
+        GameRoom.Theme = Mode;
+        GameRoom.OnEvent += (_, e) =>
         {
-            factory = new TestUserFactory();
-            GameRoom = new GameRoom(0, factory.NewUser())
-            {
-                LeaderIsPlayer = false,
-            };
-            Theme = themeConstructor(GameRoom, factory);
-            GameRoom.Theme = Theme;
-            GameRoom.OnEvent += (_, e) =>
-            {
-                lock (lockCapturedEvents)
-                    capturedEvents.Add(e);
-            };
-        }
+            lock (lockCapturedEvents)
+                capturedEvents.Add(e);
+        };
+    }
 
-        public UserInfo AddUser()
+    public UserInfo AddUser()
+    {
+        var user = factory.NewUser();
+        _ = GameRoom.AddParticipant(user);
+        return user;
+    }
+
+    public bool RemoveUser(int index)
+    {
+        var (_, entry) = GameRoom.Users.ElementAt(index);
+        return GameRoom.RemoveParticipant(entry.User);
+    }
+
+    public void InitUser(int newUserCount)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(newUserCount);
+        if (GameRoom.Users.Count > newUserCount)
+        {
+            var users = GameRoom.Users
+                .Select(x => x.Value.User)
+                .ToArray();
+            foreach (var user in users)
+            {
+                if (GameRoom.Users.Count > newUserCount)
+                {
+                    _ = GameRoom.RemoveParticipant(user);
+                }
+                else break;
+            }
+        }
+        while (GameRoom.Users.Count < newUserCount)
+        {
+            _ = GameRoom.AddParticipant(factory.NewUser());
+        }
+    }
+
+    public Runner<TMode> InitChars<TChar>(int count)
+        where TChar : Character
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(count);
+        var name = Mode.GetCharacterName(typeof(TChar));
+        IsNotNull(name, $"Character {typeof(TChar).Name} must be registered");
+        foreach (var (id, entry) in GameRoom.Users)
+        {
+            if (id == GameRoom.Leader && !GameRoom.LeaderIsPlayer)
+                continue;
+            if (count == 0)
+                break;
+            if (entry.Character is not null)
+                continue;
+            entry.Character = Mode.CreateCharacter(name);
+            count--;
+        }
+        for (int i = 0; i < count; ++i)
         {
             var user = factory.NewUser();
-            GameRoom.AddParticipant(user);
-            return user;
+            _ = GameRoom.AddParticipant(user);
+            GameRoom.Users[user.Id].Character = Mode.CreateCharacter(name);
         }
+        _ = GameRoom.RoleConfiguration.AddOrUpdate(name, _ => count, (_, old) => old + count);
+        return this;
+    }
 
-        public bool RemoveUser(int index)
+    public Runner<TMode> DefaultConfig()
+    {
+        GameRoom.LeaderIsPlayer = true;
+        GameRoom.DeadCanSeeAllRoles = true;
+        GameRoom.AutostartVotings = true;
+        GameRoom.UseVotingTimeouts = true;
+        GameRoom.AutoFinishRounds = true;
+        return this;
+    }
+
+    public bool HasCapturedEvents()
+        => capturedEvents.Count > 0;
+
+    public bool HasCapturedEvents<TGameEvent>()
+        where TGameEvent : GameEvent
+        => capturedEvents.Any(x => x is TGameEvent);
+
+    public bool HasCapturedEvents<TGameEvent>(Func<TGameEvent, bool> check)
+        where TGameEvent : GameEvent
+        => capturedEvents
+            .Where(x => x is TGameEvent)
+            .Cast<TGameEvent>()
+            .Any(check);
+
+    public void CollectEvent<TGameEvent>()
+        where TGameEvent : GameEvent
+        => CollectEvent<TGameEvent>(x => true);
+
+    public void CollectEvent<TGameEvent>(Func<TGameEvent, bool> selector)
+        where TGameEvent : GameEvent
+    {
+        for (int i = 0; i < capturedEvents.Count; ++i)
         {
-            var (_, entry) = GameRoom.Users.ElementAt(index);
-            return GameRoom.RemoveParticipant(entry.User);
+            if (capturedEvents[i] is not TGameEvent @event || !selector(@event))
+                continue;
+            capturedEvents.RemoveAt(i);
+            return;
         }
+        throw new KeyNotFoundException($"Event {typeof(TGameEvent).FullName} is not captured");
+    }
 
-        public void InitUser(int newUserCount)
-        {
-            if (newUserCount < 0)
-                throw new ArgumentOutOfRangeException(nameof(newUserCount));
-            if (GameRoom.Users.Count > newUserCount)
-            {
-                var users = GameRoom.Users
-                    .Select(x => x.Value.User)
-                    .ToArray();
-                foreach (var user in users)
-                {
-                    if (GameRoom.Users.Count > newUserCount)
-                    {
-                        GameRoom.RemoveParticipant(user);
-                    }
-                    else break;
-                }
-            }
-            while (GameRoom.Users.Count < newUserCount)
-            {
-                GameRoom.AddParticipant(factory.NewUser());
-            }
-        }
+    public void ClearEvents()
+        => capturedEvents.Clear();
 
-        public Runner<T> InitRoles<TRole>(TRole role, int count)
-            where TRole : Character
-        {
-            if (count < 0)
-                throw new ArgumentOutOfRangeException(nameof(count));
-            foreach (var (id, entry) in GameRoom.Users)
-            {
-                if (id == GameRoom.Leader && !GameRoom.LeaderIsPlayer)
-                    continue;
-                if (count == 0)
-                    break;
-                if (entry.Role is not null)
-                    continue;
-                entry.Role = role.CreateNew();
-                count--;
-            }
-            for (int i = 0; i < count; ++i)
-            {
-                var user = factory.NewUser();
-                GameRoom.AddParticipant(user);
-                GameRoom.Users[user.Id].Role = role.CreateNew();
-            }
-            Character? key = GameRoom.RoleConfiguration.Keys
-                .Where(x => x is TRole)
-                .FirstOrDefault();
-            if (key is null)
-                GameRoom.RoleConfiguration.TryAdd(role, count);
-            else GameRoom.RoleConfiguration.AddOrUpdate(key, _ => count, (_, old) => old + count);
-            return this;
-        }
+    public void ClearEvents<TGameEvent>()
+        where TGameEvent : GameEvent
+        => ClearEvents<TGameEvent>(x => true);
 
-        public Runner<T> InitRoles<TRole>(int count)
-            where TRole : Character
-        {
-            var prototype = (TRole?)Activator.CreateInstance(
-                typeof(TRole),
-                Theme
-            );
-            if (prototype is null)
-                throw new ArgumentException($"Type {typeof(TRole).FullName} has no constructor that expects one param of type {typeof(GameMode).FullName}");
-            return InitRoles(prototype, count);
-        }
-
-        public bool HasCapturedEvents()
-            => capturedEvents.Count > 0;
-
-        public bool HasCapturedEvents<E>()
-            where E : GameEvent
-            => capturedEvents.Where(x => x is E).Any();
-
-        public bool HasCapturedEvents<E>(Func<E, bool> check)
-            where E : GameEvent
-            => capturedEvents
-                .Where(x => x is E)
-                .Cast<E>()
-                .Where(check)
-                .Any();
-
-        public void CollectEvent<E>()
-            where E : GameEvent
-            => CollectEvent<E>(x => true);
-
-        public void CollectEvent<E>(Func<E, bool> selector)
-            where E : GameEvent
-        {
-            for (int i = 0; i < capturedEvents.Count; ++i)
-            {
-                if (capturedEvents[i] is not E @event || !selector(@event))
-                    continue;
-                capturedEvents.RemoveAt(i);
-                return;
-            }
-            throw new KeyNotFoundException($"Event {typeof(E).FullName} is not captured");
-        }
-
-        public void ClearEvents()
-            => capturedEvents.Clear();
-
-        public void ClearEvents<E>()
-            where E : GameEvent
-            => ClearEvents<E>(x => true);
-
-        public void ClearEvents<E>(Func<E, bool> selector)
-            where E : GameEvent
-        {
-            capturedEvents.RemoveAll(x => x is E @event && selector(@event));
-        }
+    public void ClearEvents<TGameEvent>(Func<TGameEvent, bool> selector)
+        where TGameEvent : GameEvent
+    {
+        _ = capturedEvents.RemoveAll(x => x is TGameEvent @event && selector(@event));
     }
 }
