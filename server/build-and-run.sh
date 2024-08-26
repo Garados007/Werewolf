dba6576a409bd68fde8099f73566adb7d7a24bd4
Max Brauer <ma.brauer@live.de>
2024-08-26 11:01:34 +0200

    Tools: fix paths in server/build-and-run.sh

--- server/build-and-run.sh
+++ server/build-and-run.sh
@@ -2,14 +2,21 @@
 
 set -e
 
-dotnet run --project ../tools/LogicCompiler/LogicCompiler.csproj -- \
-    -s ../logic/werewolf/ \
-    -t ../tools/bin \
+dotnet run --project ../../tools/LogicCompiler/LogicCompiler.csproj -- \
+    -s ../../logic/werewolf/ \
+    -t ../../tools/bin \
     -n "Theme.werewolf" \
     --write-ast --write-docs
-dotnet build ../tools/bin -o bin/Debug/net8.0
+if [ ! -f "../../tools/bin/bin.csproj" ]; then
+    pushd ../../tools/bin
+    dotnet new classlib
+    rm Class1.cs
+    dotnet add reference ../../server/Werewolf.Theme.Base/Werewolf.Theme.Base.csproj
+    popd
+fi
+dotnet build ../../tools/bin -o bin/Debug/net8.0
 
-set -a && source ../.env && set +a
+set -a && source ../../.env && set +a
 export GAME_SERVER_PLUGINS=bin.dll
 export GAME_SERVER_WEBSERVER_PORT=8015
 export GAME_SERVER_DOMAIN=http://localhost:8015
