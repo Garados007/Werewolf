f52e174dcd6438f7a921e2917bfe9ba785f2f27c
Max Brauer <ma.brauer@live.de>
2024-08-26 17:20:52 +0200

    Tools: update inc-version script
    this script does now commit creation and tagging

--- inc-version.sh
+++ inc-version.sh
@@ -4,3 +4,9 @@ prefix="$(sed -E "s/^([0-9\.]+\.)[0-9]+$/\1/g" version.txt)"
 let "suffix=suffix+1"
 echo -n "$prefix$suffix" > version.txt
 echo "New version is $(cat version.txt)"
+
+git add version.txt
+git commit -m "Move version to $(cat version.txt)"
+git push
+git tag -a "$(cat version.txt)" -m "Version $(cat version.txt)"
+git push origin "$(cat version.txt)"
